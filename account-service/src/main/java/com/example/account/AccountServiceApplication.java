1622862eeccb11a735a1f6cc5dd6cf6903a2c103
nvminh162 <nvminh162@gmail.com>
2026-01-22 17:36:12 +0700

    Add Kafka topic configurations and enhance account service
    - Updated `.gitignore` to include `.vscode/` directory.
    - Added Kafka topic beans for `notification` and `statistic` in `AccountServiceApplication`.
    - Enhanced `AccountController` to send messages to Kafka topics upon account creation.
    - Introduced `AccountDTO`, `MessageDTO`, and `StatisticDTO` models with necessary fields and annotations.
    - Updated `application.yaml` with Kafka producer configurations for better event handling.

--- account-service/src/main/java/com/example/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/example/account/AccountServiceApplication.java
@@ -1,7 +1,9 @@
 package com.example.account;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountServiceApplication {
@@ -10,4 +12,15 @@ public class AccountServiceApplication {
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
 
+	@Bean
+	NewTopic notification() {
+		// topic name, partition numbers, replication number
+		return new NewTopic("notification", 2, (short) 1);
+	}
+
+	@Bean
+	NewTopic statistic() {
+		// topic name, partition numbers, replication number
+		return new NewTopic("statistic", 1, (short) 1);
+	}
 }
